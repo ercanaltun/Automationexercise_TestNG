0775ce7431ec5e9f2a9d6a4d634ed794c004aa4b
Ercan ALTUN <salsel0147@gmail.com>
2023-01-13 12:39:08 +0300

    13.01.2023p.m

--- src/test/java/pages/Auto.java
+++ src/test/java/pages/Auto.java
@@ -1,6 +1,14 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Auto {
+
+    public Auto(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
 
 
 }
