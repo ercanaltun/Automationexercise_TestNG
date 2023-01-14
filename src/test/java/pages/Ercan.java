a80cc068d4ef748a853cf30d0bb9cecc48a76e7e
Ercan ALTUN <salsel0147@gmail.com>
2023-01-14 23:14:29 +0300

    14.01.2023p.m

---
+++ src/test/java/pages/Ercan.java
@@ -0,0 +1,46 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class Ercan {
+
+    public Ercan(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
